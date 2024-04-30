9a7ed2a53be18df2e49961ca55e4251b768142a8
Devendra khatri <devendrakhatri904@gmail.com>
2024-04-30 19:51:23 +0545

    completed sorting with array

---
+++ practicequestions/sort.js
@@ -0,0 +1,20 @@
+const people = [
+  { name: "devendra", age: 30 },
+  { name: "snigdha", age: 20 },
+  { name: "ram", age: 10 },
+];
+
+let result = people.sort((a, b) => {
+  return a.age - b.age;
+});
+
+console.log(result);
+
+let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+number.sort((a, b) => {});
+//const data=[]
+//const page;
+//const limit;
+// write a js function that takes in data,page and limit and return the data
+//[3,4]
