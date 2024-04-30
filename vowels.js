183c42a6bad8953d81e37e4f924d930752142e68
Devendra <devendrakhatri904@gmail.com>
2024-04-30 14:23:58 +0545

    converted the approch using match()

--- vowels.js
+++ vowels.js
@@ -1,20 +1,3 @@
 const str = "Abcdefghijklmnopqrstuvwxyz";
-strLwr = str.toLowerCase();
-
-let charArray = strLwr.split("");
-let vowels = 0,
-  consonant = 0;
-charArray.map((items, index) => {
-  if (
-    items === "a" ||
-    items === "e" ||
-    items === "i" ||
-    items === "o" ||
-    items === "u"
-  ) {
-    vowels++;
-  } else {
-    consonant++;
-  }
-});
-console.log(vowels);
+let checkVowels = str.match(/[aeiou]/gi);
+console.log(checkVowels);
