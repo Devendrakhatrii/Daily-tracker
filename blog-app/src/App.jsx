84c11f43c6d23a9b9ce0ce8d83132d8b2f981900
Devendra <devendrakhatri904@gmail.com>
2024-05-09 15:04:57 +0545

    added logo files

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -1,10 +1,12 @@
 import { CardWithForm } from "./components/CardWithForm";
+import { ChangePassword } from "./components/ChangePassword";
 
 function App() {
   return (
     <>
       <div className="w-screen h-screen  flex items-center justify-center">
-        <CardWithForm />
+        {/* <CardWithForm /> */}
+        <ChangePassword />
       </div>
     </>
   );
