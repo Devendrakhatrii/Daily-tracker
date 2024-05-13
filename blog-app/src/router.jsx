f9756a9ca84f6cbb75fc42e83f4424a31a591608
Devendra <devendrakhatri904@gmail.com>
2024-05-13 19:49:20 +0545

    tried figuring out the error

--- blog-app/src/router.jsx
+++ blog-app/src/router.jsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from "@/pages/Home";
-import LoginPage from "@/pages/Login";
+import { LoginPage } from "@/pages/Login";
 import { SignupPage } from "@/pages/Signup";
 import { DashboardPage } from "@/pages/Dashboard";
+import { NavigationPage } from "@/pages/Navigation";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: "welcome",
+    element: <NavigationPage />,
   },
   {
     path: "/login",
