79a9f7facea0c106673ddb924b64f3abdfe5005b
Devendra <devendrakhatri904@gmail.com>
2024-05-13 16:39:29 +0545

    configured react router with login and signup page

--- blog-app/src/router.jsx
+++ blog-app/src/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/Home";
-import LoginPage from "./pages/Login";
+import HomePage from "@/pages/Home";
+import LoginPage from "@/pages/Login";
+import { SignupPage } from "@/pages/Signup";
 
 export const router = createBrowserRouter([
   {
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
   {
     path: "/login",
     element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
   },
 ]);
