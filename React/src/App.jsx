aa867ecdf40b0477e2c80a724f77fbf0a2faad57
Devendra khatri <devendrakhatri904@gmail.com>
2024-05-14 21:28:26 +0545

    worked with react on Day-1

---
+++ React/src/App.jsx
@@ -0,0 +1,13 @@
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+const App = () => {
+  return (
+    <>
+      <About />
+      <Contact />
+    </>
+  );
+};
+
+export default App;
