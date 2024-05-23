e94d4feaef11c3f72fbdaeb9a00eb8a957894544
Devendra <devendrakhatri904@gmail.com>
2024-05-23 12:57:10 +0545

    created a basic receipe app with no search functionality

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,35 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [recipes, setRecipes] = useState([]);
+  const [ingredients, setIngredients] = useState(false);
+  const [instructions, setInstructions] = useState(false);
+  const [rating, setRating] = useState(false);
+  const [search, setSearch] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          "https://dummyjson.com/recipes?limit=50&skip=10&select=name,image,ingredients,instructions,rating"
+        );
+        const data = await response.json();
+        setRecipes(data.recipes);
+        console.log(data.recipes);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getData();
+  }, []);
+
+  const searchRecipe = (query) => {
+    let result = recipes.includes(query);
+    console.log(result);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Receipe App</h1>
+        <input
+          type="text"
+          placeholder="Search receipe"
+          onChange={(e) => searchRecipe(e.target.value)}
+        />
+        <input type="checkbox" name="name" id="" checked disabled />
+        <label htmlFor="name">Name</label>
+        <input type="checkbox" name="image" id="" checked disabled />
+        <label htmlFor="image">image</label>
+        <input
+          type="checkbox"
+          name=""
+          id=""
+          onChange={() => setIngredients(!ingredients)}
+        />
+        <label htmlFor="ingredients">ingredients</label>
+        <input
+          type="checkbox"
+          name="instrustions"
+          id=""
+          onChange={() => setInstructions(!instructions)}
+        />
+        <label htmlFor="instructions">instructions</label>
+        <input
+          type="checkbox"
+          name="rating"
+          id=""
+          checked
+          onChange={() => setRating(!rating)}
+        />
+        <label htmlFor="rating">rating</label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        {recipes.map((item, index) => {
+          return (
+            <>
+              <div>
+                <li key={index}>
+                  <h1>{item.name}</h1>
+                </li>
+                <img src={`${item.image}`} alt="" id="recipe-img" />
+              </div>
+              {ingredients && (
+                <div>
+                  <h3>Ingrediants</h3>
+                  {item.ingredients.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </div>
+              )}
+              {instructions && (
+                <div>
+                  <h3>Instructions</h3>
+                  {item.instructions.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </div>
+              )}
+              {rating && (
+                <div>
+                  <h3>Rating : {item.rating}</h3>
+                </div>
+              )}
+            </>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
